0ec2df9072cd0539c70cf4d7aee12c9af70b0ce6
laraujo <laraujo@zak.app>
2022-03-05 19:06:25 -0300

    feat(handler.cuboids): implement cuboids update and delete

--- app/api/handler/cuboid.py
+++ app/api/handler/cuboid.py
@@ -64,3 +64,54 @@ def create_cuboid():
     db.session.commit()
 
     return jsonify(cuboid_schema.dump(cuboid)), HTTPStatus.CREATED
+
+
+@cuboid_api.route("/<int:cuboid_id>", methods=["PUT"])
+def update_cuboid(cuboid_id):
+    content = request.json
+
+    width = content.get("width")
+    height = content.get("height")
+    depth = content.get("depth")
+    bag_id = content.get("bag_id")
+    # todo: validate each attribute before updating them
+
+    cuboid = Cuboid.query.get(cuboid_id)
+    if not cuboid:
+        return {
+                   "error": f"Cuboid with id #{cuboid_id} does not exist."
+        }, HTTPStatus.NOT_FOUND
+
+    cuboid.width = width
+    cuboid.height = height
+    cuboid.depth = depth
+
+    # Todo: break into isolated functions
+    if bag_id:
+        bag = Bag.query.get(bag_id)
+        if not bag:
+            return {
+                       "error": f"Bag with id #{bag_id} does not exist."
+            }, HTTPStatus.NOT_FOUND
+
+        cuboid.bag_id = bag_id
+
+    db.session.add(cuboid)
+    db.session.commit()
+
+    cuboid_schema = CuboidSchema()
+    return jsonify(cuboid_schema.dump(cuboid)), HTTPStatus.OK
+
+
+@cuboid_api.route("/<int:cuboid_id>", methods=["DELETE"])
+def delete_cuboid(cuboid_id):
+    cuboid = Cuboid.query.get(cuboid_id)
+
+    if not cuboid:
+        return {
+                   "error": f"Cuboid with id #{cuboid_id} does not exist."
+        }, HTTPStatus.NOT_FOUND
+
+    cuboid.query.delete()
+    db.session.commit()
+    return {}, HTTPStatus.OK
